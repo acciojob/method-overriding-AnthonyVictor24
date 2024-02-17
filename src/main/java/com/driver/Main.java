c62d1fe9175116889604701f4c4f8048e3ffd08e
AnthonyVictor24 <arogyaanthonyvictor24@gmail.com>
2024-02-17 12:36:36 +0530

    OverRiden Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-  
+  public static class A{
+      public String meth(){
+          return "Iam in classA";
+      }
+  }
+  public static class B extends A{
+      public String meth(){
+          return "Method is overridden in Extendend class B";
+      }
+  }
+
+    public static void main(String[] args) {
+        A obj = new B();
+        System.out.println(obj.meth());
+    }
 }
